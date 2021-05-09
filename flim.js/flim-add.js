0638495db7082ed5250a7c9cb5b1749569538402
artegoser <59178854+artegoser@users.noreply.github.com>
2021-05-09 10:57:04 +0300

    check package.json

--- flim.js/flim-add.js
+++ flim.js/flim-add.js
@@ -2,40 +2,54 @@ const { spawn } = require("child_process");
 const logUpdate = require('log-update');
 const chalk = require('chalk');
 const fetch = require('node-fetch');
-
+const logger = require("./modules/flim-logger")
 const fs = require("fs");
 
-function npm(pkg, g){
+function npm(pkg, g, resolve){
         let xc = g ? ["i"].concat(pkg,'-g') : ["i"].concat(pkg);
         const sp = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', xc);
 
         sp.stdout.on("data", data => {
-            console.log(`${data}`);
+            logUpdate(`  ${data}`.replace(/\n/g, "\n    "));
+            logUpdate.done()
         });
         
         sp.stderr.on("data", data => {
-            console.log(`\u001b[41mflim warn:\u001b[0m ${data}`);
+            logUpdate(`  ${data}`.replace(/\n/g, "\n    "));
+            logUpdate.done();
         });
         
         sp.on('error', (error) => {
-            console.log(`\u001b[41mflim warn:\u001b[0m ${error.message}`);
+            logUpdate(`  ${error.message}`.replace(/\n/g, "\n    "));
+            logUpdate.done();
+        });
+
+        sp.on('exit', ()=>{
+            if(resolve) resolve();
         });
 }
 
-function yarn(pkg){
+function yarn(pkg, resolve){
     let xc = ["add"].concat(pkg)
     const sp = spawn(/^win/.test(process.platform) ? 'yarn.cmd' : 'yarn', xc);
 
     sp.stdout.on("data", data => {
-        console.log(`${data}`);
+        logUpdate(`  ${data}`.replace(/\n/g, "\n    "));
+        logUpdate.done()
     });
     
     sp.stderr.on("data", data => {
-        console.log(`\u001b[41mflim warn:\u001b[0m ${data}`);
+        logUpdate(`  ${data}`.replace(/\n/g, "\n    "));
+        logUpdate.done();
     });
     
     sp.on('error', (error) => {
-        console.log(`\u001b[41mflim warn:\u001b[0m ${error.message}`);
+        logUpdate(`  ${error.message}`.replace(/\n/g, "\n    "));
+        logUpdate.done();
+    });
+
+    sp.on('exit', ()=>{
+        if(resolve) resolve();
     });
 }
 
@@ -44,118 +58,84 @@ class flim{
         pkg = Array.isArray(pkg) ? pkg[0] : pkg;
         this.i = 0;
         this.spinframes = process.platform !== 'win32' ? ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'] : ['-', '\\', '|', '/'];
-
-        this.info(`flim version ${require("../package.json").version}`);
-        this.info(`The global flim index doesn't work in flim yet`);
+        this.mainlogger = new logger();
+        this.mainlogger.info(`flim version ${require("../package.json").version}`);
+        this.mainlogger.info(`The global flim index doesn't work in flim yet`);
         this.index = {};
         try{
-          this.info(`Try to read flim-index.json in local storage`);
+          this.mainlogger.info(`Try to read flim-index.json in local storage`);
           this.index = fs.readFileSync(`${__dirname}\\flim-index.json`);
           this.flimpkg(this.index[pkg], pkg, g);
         } catch{
-          this.info(`Local index not found, using cdn.jsdelivr.net`);
-          this.info(`Getting ${pkg} from https://cdn.jsdelivr.net/npm/${pkg}/flim.json`);
+          this.mainlogger.info(`Local index not found, using cdn.jsdelivr.net`);
+          this.mainlogger.info(`Getting ${pkg} from https://cdn.jsdelivr.net/npm/${pkg}/flim.json`);
           fetch(`https://cdn.jsdelivr.net/npm/${pkg}/flim.json`)
             .then(res => res.json())
             .then(json => {
                 this.index[pkg] = json;
                 this.flimpkg(this.index[pkg], pkg, g);
-            }).catch(err=> {throw new Error(err)});
+            }).catch(err=> {
+                this.mainlogger.warn("Fetch Error"); 
+                this.mainlogger.info("Trying to get package.json"); 
+                fetch(`https://cdn.jsdelivr.net/npm/${pkg}/package.json`)
+                .then(res => res.json())
+                .then(json => {
+                    this.index[pkg] = {};
+                    this.index[pkg].type = "npm-node";
+                    this.flimpkg(this.index[pkg], pkg, g);
+                }).catch(()=>{
+                    this.mainlogger.warn("Fetch Error"); 
+                    console.log(err); 
+                    process.exit(1);
+                });
+            });
         }
     }
     flimpkg(json, pkg, g){
         if(json.type=="flim"){
-            this.warn("Sorry, flim type still in development")
+            this.mainlogger.warn("Sorry, flim type still in development")
             //later
         } else if(json.type=="npm-node"){
-            this.startFunc(`npm-node setup for ${pkg}`, ctx=>{
-                let xc = g ? ["i"].concat(pkg,'-g') : ["i"].concat(pkg);
-                const sp = spawn(/^win/.test(process.platform) ? 'npm.cmd' : 'npm', xc);
-                sp.stdout.on("data", data => {
-                    ctx.log(`  ${data}`.replace(/\n/g, "\n    "));
+            this.startFunc(`npm-node setup for ${pkg}`, async ctx=>{
+                await new Promise((res)=>{
+                    npm(pkg, g, res);
                 });
-                sp.stderr.on("data", data => {
-                    ctx.log(`  \u001b[41mflim warn:\u001b[0m ${data}`.replace(/\n/g, "\n    "));
-                });
-                sp.on('error', (error) => {
-                    ctx.log(`  \u001b[41mflim warn:\u001b[0m ${error.message}`.replace(/\n/g, "\n    "));
-                });
-                sp.on('exit', ()=>{
-                    ctx.done();
-                });
+                ctx.done();
             });
         } else if(json.type=="yarn-node"){
-            this.startFunc(`npm-yarn setup for ${pkg}`, ctx=>{
-                let xc = g ? ["add"].concat(pkg) : ["add"].concat(pkg);
-                const sp = spawn(/^win/.test(process.platform) ? 'yarn.cmd' : 'yarn', xc);
-                sp.stdout.on("data", data => {
-                    ctx.log(`  ${data}`.replace(/\n/g, "\n    "));
+            this.startFunc(`npm-yarn setup for ${pkg}`, async ctx=>{
+                await new Promise((res)=>{
+                    yarn(pkg, res);
                 });
-                sp.stderr.on("data", data => {
-                    ctx.log(`  \u001b[41mflim warn:\u001b[0m ${data}`.replace(/\n/g, "\n    "));
-                });
-                sp.on('error', (error) => {
-                    ctx.log(`  \u001b[41mflim warn:\u001b[0m ${error.message}`.replace(/\n/g, "\n    "));
-                });
-                sp.on('exit', ()=>{
-                    ctx.done();
-                });
+                ctx.done();
             });
         } else{
-            this.warn(`this type (${json.type}) is not supported`)
+            this.warn(`this type (${json.type}) is not supported`);
         }
     }
-    startFunc(title,func, brackets=true){
+    async startFunc(title,func,brackets=true){
         if(brackets) console.log(chalk.yellow(`flim run:  ${title} {`));
         else console.log(chalk.yellow(`flim run:  ${title} `))
+        const funclogger = new logger("  ");
         let time = setInterval(()=>{
             this.next_frame();
             logUpdate(chalk.yellow(`${this.frame} ${title}`));
         }, 150);
-
-        function warn(msg){
-            logUpdate("  "+chalk.black.bgWhite(`${chalk.bgRed.white("flim warn:")} ${msg} `));
-            logUpdate.done();
-        }
-        function ok(msg){
-            logUpdate("  "+chalk.black.bgWhite(`${chalk.bgGreen.white("flim ok:  ")} ${msg} `));
-            logUpdate.done();
-        }
         function done(){
             clearInterval(time);
             if(brackets) logUpdate(chalk.yellow(`}`));
             else logUpdate();
             logUpdate.done();
         }
-        function info(msg){
-            logUpdate("  "+chalk.black.bgWhite(`${chalk.bgBlue.white("flim info:")} ${msg} `));
-            logUpdate.done();
-        }
-        function log(msg){
-            logUpdate(msg);
-            logUpdate.done();
-        }
 
         let context = {
-            warn:warn,
-            ok:ok,
+            warn:funclogger.warn,
+            ok:funclogger.ok,
             done:done,
-            info:info,
-            log:log
+            info:funclogger.info,
+            log:funclogger.log
         }
-        func(context);
-    }
-    warn(msg){
-        logUpdate.stderr(chalk.black.bgWhite(`${chalk.bgRed.white("flim warn:")} ${msg} `));
-        logUpdate.stderr.done();
-    }
-    info(msg){
-        logUpdate(chalk.black.bgWhite(`${chalk.bgBlue.white("flim info:")} ${msg} `));
-        logUpdate.done();
-    }
-    ok(msg){
-        logUpdate(chalk.black.bgWhite(`${chalk.bgGreen.white("flim ok:  ")} ${msg} `));
-        logUpdate.done();
+        await func(context);
     }
     get frame(){
         return this.spinframes[this.i];
---
+++ flim.js/modules/flim-logger.js
@@ -0,0 +1,26 @@
+const chalk = require('chalk');
+const logUpdate = require('log-update');
+
+class Logger{
+    constructor(tab=""){
+        this.tab = tab;
+    }
+    warn(msg){
+        logUpdate(this.tab+chalk.black.bgWhite(`${chalk.bgRed.white("flim warn:")} ${msg} `));
+        logUpdate.stderr.done();
+    }
+    info(msg){
+        logUpdate(this.tab+chalk.black.bgWhite(`${chalk.bgBlue.white("flim info:")} ${msg} `));
+        logUpdate.done();
+    }
+    ok(msg){
+        logUpdate(this.tab+chalk.black.bgWhite(`${chalk.bgGreen.white("flim ok:  ")} ${msg} `));
+        logUpdate.done();
+    }
+    log(msg){
+        logUpdate(this.tab+msg);
+        logUpdate.done();
+    }
+}
+
+module.exports = Logger;
