a8e5d3c32cf8108e1eb8adeca20a68737736b1a5
artegoser <59178854+artegoser@users.noreply.github.com>
2021-05-10 09:59:02 +0300

    futurize

--- flim.js/modules/flim-patch-commander.js
+++ flim.js/modules/flim-patch-commander.js
@@ -1,7 +1,7 @@
 function patchCommander(Command) {
     Command.prototype.forwardSubcommands = function() {
-        var self = this;
-        var listener = function(args, unknown) {
+        let self = this;
+        let listener = function(args, unknown) {
             args = args || [];
             unknown = unknown || [];
             var parsed = self.parseOptions(unknown);
@@ -16,8 +16,8 @@ function patchCommander(Command) {
         if (this._args.length > 0) {
             console.error('forwardSubcommands cannot be applied to command with explicit args');
         }
-        var parent = this.parent || this;
-        var name = parent === this ? '*' : this._name;
+        let parent = this.parent || this;
+        let name = parent === this ? '*' : this._name;
         parent.on('command:' + name, listener);
         if (this._alias) parent.on('command:' + this._alias, listener);
         return this;
