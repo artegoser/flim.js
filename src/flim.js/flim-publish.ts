7f1e44575ce66dffd38bc55583df3a282e283b08
artegoser <59178854+artegoser@users.noreply.github.com>
2021-05-18 21:55:13 +0300

    password security

--- src/flim.js/flim-publish.ts
+++ src/flim.js/flim-publish.ts
@@ -3,19 +3,36 @@ import * as Api from "flim-api";
 import * as readline from 'readline';
 import * as fs from "fs";
 import { Logger } from "./modules/flim-logger";
+import { Writable } from 'stream';
 
 async function publish() {
     let logger = new Logger();
+    let muted = false;
+    let mutableStdout = new Writable({
+        write: function(chunk, encoding, callback) {
+            if (!muted) process.stdout.write(chunk, encoding);
+            else process.stdout.write(Buffer.from("\x1B[2K\x1B[200D"+"password: "+"*".repeat(rl.line.length), "utf-8"), encoding);
+            callback();
+        }
+    });
+
     const rl = readline.createInterface({
         input: process.stdin,
-        output: process.stdout
+        output: mutableStdout,
+        terminal: true
     });
+
     let name:string = await new Promise(resolve => {
         rl.question("name: ", resolve);
     }) || "";
+
+   
     let password:string = await new Promise(resolve => {
         rl.question("password: ", resolve);
+        muted=true;
     }) || "";
+    console.log();
+    
     if(!(name||password)) {
         logger.warn("Unspecified parameter");
         process.exit(1);
@@ -36,12 +53,12 @@ async function publish() {
                 ctx.warn(JSON.stringify(res));
                 ctx.done();
             } else{
+                ctx.ok(res.message);
                 ctx.done();
-                logger.ok(res.message);
             }
             console.timeEnd("final");
             process.exit(0);
-        });
+        }, "", false);
     } else{
         logger.warn("Some parameters are not specified");
     }
